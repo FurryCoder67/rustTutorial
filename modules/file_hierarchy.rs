9a483138749f893b8f933769c184d8b1e9b9d135
Aiden <aiden@rivi.us>
2026-05-23 14:52:45 -0400

    a

---
+++ modules/file_hierarchy.rs
@@ -0,0 +1,66 @@
+$ tree .
+.
+├── my
+│   ├── inaccessible.rs
+│   └── nested.rs
+├── my.rs
+└── split.rs
+
+
+// This declaration will look for a file named `my.rs` and will
+// insert its contents inside a module named `my` under this scope
+mod my;
+
+fn function() {
+    println!("called `function()`");
+}
+
+fn main() {
+    my::function();
+
+    function();
+
+    my::indirect_access();
+
+    my::nested::function();
+}
+
+// Similarly `mod inaccessible` and `mod nested` will locate the `nested.rs`
+// and `inaccessible.rs` files and insert them here under their respective
+// modules
+mod inaccessible;
+pub mod nested;
+
+pub fn function() {
+    println!("called `my::function()`");
+}
+
+fn private_function() {
+    println!("called `my::private_function()`");
+}
+
+pub fn indirect_access() {
+    print!("called `my::indirect_access()`, that\n> ");
+
+    private_function();
+}
+
+pub fn function() {
+    println!("called `my::nested::function()`");
+}
+
+#[allow(dead_code)]
+fn private_function() {
+    println!("called `my::nested::private_function()`");
+}
+
+#[allow(dead_code)]
+pub fn public_function() {
+    println!("called `my::inaccessible::public_function()`");
+}
+$ rustc split.rs && ./split
+called `my::function()`
+called `function()`
+called `my::indirect_access()`, that
+> called `my::private_function()`
+called `my::nested::function()`
