0d0c9619512831acb1f9b89f59bbfd525398d06e
Aiden <aiden@rivi.us>
2026-05-23 14:31:13 -0400

    a

---
+++ conversion/from_and_into.rs
@@ -0,0 +1,58 @@
+let my_str = "hello";
+let my_string = String::from(my_str);
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+fn main() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+}
+use std::convert::Into;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl Into<Number> for i32 {
+    fn into(self) -> Number {
+        Number { value: self }
+    }
+}
+
+fn main() {
+    let int = 5;
+    // Try removing the type annotation
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+// Define `From`
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+fn main() {
+    let int = 5;
+    // use `Into`
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
